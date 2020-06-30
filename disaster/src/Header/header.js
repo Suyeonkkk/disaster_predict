b8e63bb9158ff1d275acce17fac395e319933f29
201502029 <tnehd158@naver.com>
2020-06-30 16:20:47 +0900

    create header

---
+++ disaster/src/Header/header.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./header.css";
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <div classNAme="line block">
+            <div className="headline">
+                <div className="headlinelogo headfactor">
+                    <Link to="/" className="none">
+                        <img className="headicon headfactor"
+                        src="https://image.flaticon.com/icons/svg/126/126474.svg"
+                        alt="main icon"
+                        title="Disaster Predict Service"/>
+                    </Link>
+                </div>
+                <div className="headlineexplanation headfactor">
+                    <Link to="/" className="none">
+                        <p className="header">Disaster</p>
+                        <p className="black">Predict</p>
+                    </Link> 
+                </div>
+                <div className="rightside">
+                    <div className="headfactor headbutton">
+                        <Link to="/" className="none">
+                            <p className="height5">Header</p>
+                        </Link>
+                    </div>
+                </div>
+                <div className="rightside">
+                    <div className="headfactor headbutton">
+                        <Link to="/" className="none">
+                            <p className="height5">Header</p>
+                        </Link>
+                    </div>
+                </div>
+                <div className="rightside">
+                    <div className="headfactor headbutton">
+                        <Link to="/" className="none">
+                            <p className="height5">Header</p>
+                        </Link>
+                    </div>
+                </div>
+                <div className="rightside">
+                    <div className="headfactor headbutton">
+                        <Link to="/" className="none">
+                            <p className="height5">Header</p>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
