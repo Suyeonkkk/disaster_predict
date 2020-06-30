8c2bd71eb9851415e58c9e34eb1245a6c1371eea
201502029 <tnehd158@naver.com>
2020-06-30 16:42:17 +0900

    create home

--- disaster/src/App.js
+++ disaster/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './Header/header';
+import Home from './Home/home';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Header />{/*
-          <Route exact path="/" component={ Home } />
+          <Header />
+          <Route exact path="/" component={ Home } />{/*
           <Route path="/Search" component={  } />*/}
         </div>
       </Router>
---
+++ disaster/src/Home/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+    return (
+        <div className="bodyline block">   
+            <div className="maindisplay">                
+                <h1 className="mainfont">Disaster Predict Service</h1>
+                <h4 className="subfont">
+                <br />인공지능 기술을 이용하여 재난을 예측해드립니다.</h4>
+            </div>
+        </div>   
+    );
+}
+
+export default Home;
